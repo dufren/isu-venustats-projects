12df4b5c4213c5c56d378b703237dcb127f55646
ozgur <ozgurcokckn@gmail.com>
2023-02-08 21:17:23 +0300

    welcome page

--- src/features/login/Welcome.js
+++ src/features/login/Welcome.js
@@ -1,14 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
-    return (
-        <div className='bg-slate-400 min-h-screen text-white text-center'>
-            <p className='text-2xl text-gray-700'><Link to="/devam-eden-ulusal">Devam Eden Ulusal</Link></p>
-            <p className='text-2xl mt-5 text-gray-700'><Link to="/devam-eden-uluslararası">Devam Eden Uluslararası</Link></p>
-            <p className='text-2xl mt-5 text-gray-700'><Link to="/tamamlanan-ulusal">Tamamlanan Ulusal</Link></p>
-            <p className='text-2xl mt-5 text-gray-700'><Link to="/tamamlanan-uluslararası">Tamamlanan Uluslararası</Link></p>
-        </div>
-    )
-}
+  return (
+    <div className="bg-slate-300 min-h-screen text-white text-center">
+      <p className="text-2xl text-gray-700">
+        <Link to="/devam-eden-ulusal">Devam Eden Ulusal</Link>
+      </p>
+      <p className="text-2xl mt-5 text-gray-700">
+        <Link to="/devam-eden-uluslararası">Devam Eden Uluslararası</Link>
+      </p>
+      <p className="text-2xl mt-5 text-gray-700">
+        <Link to="/tamamlanan-ulusal">Tamamlanan Ulusal</Link>
+      </p>
+      <p className="text-2xl mt-5 text-gray-700">
+        <Link to="/tamamlanan-uluslararası">Tamamlanan Uluslararası</Link>
+      </p>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
